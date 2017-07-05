97ba230398ccc8334ee56d6480823438eb7a8c66
IrisVokopola <iris@webigence.com>
2017-07-05 17:28:36 +0100

    test 1

---
+++ TestingBuildProcessTemplates/WebApplicationTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationTesting.Startup))]
+namespace WebApplicationTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
